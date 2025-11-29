b5fdc5fa984c4e32af75577a87eb1e7e2003aec7
Pierre Roy <lrp.roy@gmail.com>
2025-11-29 13:50:24 +0100

    Hot Reload + stuff

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,35 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Scripts.Player
 {
+    [RequireComponent(typeof(PlayerInput))]
     public class PlayerMovement : MonoBehaviour
     {
+        [MinValue(0)]
+        [SerializeField]
+        private float _speed;
+        
+        private PlayerInput _playerInput;
+        private Vector2 _moveInput;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _playerInput.actions.Enable();
+        }
+
+        public void OnMove(InputValue value)
+        {
+            _moveInput = value.Get<Vector2>();
+        }
+
+        private void Update()
+        {
+            var direction = new Vector3(_moveInput.x, _moveInput.y);
+            var movement = direction * _speed * Time.deltaTime;
+            gameObject.transform.position += movement;
+        }
     }
 }

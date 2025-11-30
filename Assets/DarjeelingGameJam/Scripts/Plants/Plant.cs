c66ef735a3da09b86b3789652d64e4e874f91b97
Pierre Roy <lrp.roy@gmail.com>
2025-11-30 17:49:25 +0100

    Random scale

--- Assets/DarjeelingGameJam/Scripts/Plants/Plant.cs
+++ Assets/DarjeelingGameJam/Scripts/Plants/Plant.cs
@@ -1,8 +1,18 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace DarjeelingGameJam.Plants
 {
     public class Plant : MonoBehaviour
     {
+        [Vector2AsRange]
+        [SerializeField]
+        private Vector2 _scaleMinMax = new(0.7f, 1.3f);
+        
+        private void Awake()
+        {
+            var scale = Random.Range(_scaleMinMax.x, _scaleMinMax.y);
+            transform.localScale = new Vector3(scale, scale, 0);
+        }
     }
 }

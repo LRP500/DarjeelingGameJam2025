a34bf4005ddfdc344f0aeb9588bfe944130e09eb
Pierre Roy <lrp.roy@gmail.com>
2025-11-30 11:14:17 +0100

    Camera target position limits

--- Assets/DarjeelingGameJam/Scripts/Player/PlayerMovement.cs
+++ Assets/DarjeelingGameJam/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using Sirenix.OdinInspector;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +12,14 @@ namespace DarjeelingGameJam.Player
         [SerializeField]
         private float _speed;
         
+        [Vector2AsRange]
+        [SerializeField]
+        private Vector2 _horizontalLimits = new(0, 0);
+
+        [Vector2AsRange]
+        [SerializeField]
+        private Vector2 _verticalLimits = new(0, 0);
+
         private PlayerInput _playerInput;
 
         private Camera _camera;
@@ -27,6 +36,8 @@ namespace DarjeelingGameJam.Player
             var mousePosition = Mouse.current.position.ReadValue();
             var worldPosition = _camera.ScreenToWorldPoint(mousePosition);
             worldPosition.z = _camera.nearClipPlane;
+            worldPosition.x = Mathf.Clamp(worldPosition.x, _horizontalLimits.x, _horizontalLimits.y);
+            worldPosition.y = Mathf.Clamp(worldPosition.y, _verticalLimits.x, _verticalLimits.y);
             transform.position = worldPosition;
         }
     }

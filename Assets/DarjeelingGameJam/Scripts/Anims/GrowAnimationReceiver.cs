6a0d0cdc0f473ce50042799720768d4ab1ffcc7c
Pierre Roy <lrp.roy@gmail.com>
2025-11-30 14:43:51 +0100

    Mmmmh

--- Assets/DarjeelingGameJam/Scripts/Anims/GrowAnimationReceiver.cs
+++ Assets/DarjeelingGameJam/Scripts/Anims/GrowAnimationReceiver.cs
@@ -1,19 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-/// <summary>
-/// Reçoit l'AnimationEvent "EndGrow" sur l'animation "Fleur".
-/// Mets ce script sur le même GameObject que l'Animator.
-/// </summary>
 public class GrowAnimationReceiver : MonoBehaviour
 {
-    [Tooltip("Appelé quand l'AnimationEvent 'EndGrow' est déclenché.")]
     public UnityEvent onEndGrow;
 
-    // DOIT matcher EXACTEMENT le nom de l'AnimationEvent : EndGrow
     public void EndGrow()
     {
-        Debug.Log($"[GrowAnimationReceiver] EndGrow reçu sur {gameObject.name}");
         onEndGrow?.Invoke();
     }
 }

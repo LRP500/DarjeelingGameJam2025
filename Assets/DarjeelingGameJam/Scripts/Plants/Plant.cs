15d1f8b4ff50211c6570b7437145feaa453659c5
ThomasSand <thomas.sandmeier@live.com>
2025-12-20 17:31:20 +0100

    randomize plant spawn

--- Assets/DarjeelingGameJam/Scripts/Plants/Plant.cs
+++ Assets/DarjeelingGameJam/Scripts/Plants/Plant.cs
@@ -9,10 +9,24 @@ namespace DarjeelingGameJam.Plants
         [SerializeField]
         private Vector2 _scaleMinMax = new(0.7f, 1.3f);
 
+        [SerializeField]
+        private float _maxRotationY = 10f;
+
         private void Awake()
         {
             var scale = Random.Range(_scaleMinMax.x, _scaleMinMax.y);
             transform.localScale = new Vector3(scale, scale, 0);
+
+            // Flip horizontal aléatoire pour plus de variation
+            var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.flipX = Random.value > 0.5f;
+            }
+
+            // Rotation aléatoire en Y (très légère)
+            float randomRotationY = Random.Range(-_maxRotationY, _maxRotationY);
+            transform.rotation = Quaternion.Euler(0, randomRotationY, 0);
         }
     }
 }
